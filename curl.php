d281607c82afa2bfc2942192b7065708ea3cf234
grubbywww <wangyj@easemob.com>
2016-04-21 16:44:27 +0800

    curl

--- curl.php
+++ curl.php
@@ -8,7 +8,7 @@ $f='login';
 $cookie_jar = tempnam('.','cookie');
 
 
-$post_data="j_username=".$user."&j_password=".$password;
+$post_data="j_username=".$user."&j_password=".$password."&remember_me=false&from=''";
 $h=array('Referer: http://ci-ali.easemob.com/login?from=','Origin: http://ci-ali.easemob.com','User-Agent:Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36');
 $ch = curl_init();
 
@@ -20,7 +20,7 @@ curl_setopt($ch,CURLOPT_POST,1);
 curl_setopt($ch,CURLOPT_POSTFIELDS,$post_data);
 curl_setopt($ch, CURLOPT_COOKIEJAR, $cookie_jar);
 curl_setopt($ch, CURLOPT_NOBODY, false);
-curl_exec($ch);
+$a=curl_exec($ch);
 curl_close($ch);
 
 $ch2 = curl_init();
@@ -30,6 +30,6 @@ curl_setopt($ch2, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch2,CURLOPT_HTTPHEADER,$h);
 curl_setopt($ch2, CURLOPT_COOKIEFILE, $cookie_jar);
 $orders = curl_exec($ch2);
-var_dump($orders);
+var_dump($a);
 curl_close($ch2);
 ?>
